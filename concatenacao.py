e491f7a8ae3b56266b5090e25e0be4dcb96c7e5d
Igor Manoel <igorssmanoel@gmail.com>
2025-12-08 20:14:22 +0000

    concatenacao

---
+++ concatenacao.py
@@ -0,0 +1,7 @@
+nome ="Igor" 
+sobrenome = "Manoel"
+idade = "32"
+
+print("Meu nome é " + nome + " " + sobrenome + ", eu tenho " + idade + " anos de idade")
+print("Meu nome é ",nome, sobrenome, ", eu tenho", idade, "anos de idade")
+print(f"Meu nome é {nome} {sobrenome}, eu tenho {idade} anos de idade")
